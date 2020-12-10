28543394ba8f45addad54491850f4fc0af9d23bd
SivaramPg <sivarampg95@gmail.com>
2020-12-10 17:08:08 +0530

    setup application for styled-components and remove unwanted style files

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Next.js Starter Template</title>
+      </Head>
+      <h1>Next.js Starter Template</h1>
+    </>
+  );
+}
